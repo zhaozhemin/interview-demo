76da8b12364f4280970d93aa2f24190077454104
Zhao Min <zhaozhemyn@gmail.com>
2026-04-25 14:52:58 +0800

    Add README

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.2.9", "chell"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
